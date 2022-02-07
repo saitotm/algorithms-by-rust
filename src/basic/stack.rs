75bf1e5d42bcbd91c3aeff1d501f9d89fd1945db
saitotm <tomokazu.ml.tech@gmail.com>
2022-02-07 22:08:33 +0900

    add struct item

--- src/basic/stack.rs
+++ src/basic/stack.rs
@@ -1,6 +1,23 @@
 use std::rc::Rc;
 
-pub struct Stack<T>(Option<Rc<(T, Stack<T>)>>);
+pub struct Stack<T> (Option<Rc<Item<T>>>);
+
+struct Item<T> {
+    value: T,
+    prev: Stack<T>,
+}
+
+impl<T> Item<T> {
+    fn new(value: T, prev: Stack<T>) -> Self {
+        Self { value, prev }
+    }
+}
+
+impl<T: Clone> Clone for Item<T> {
+    fn clone(&self) -> Self {
+        Self { value: self.value.clone(), prev: self.prev.clone() }
+    }
+}
 
 impl<T> Clone for Stack<T> {
     fn clone(&self) -> Self {
@@ -15,7 +32,7 @@ impl<T> Stack<T> {
     
     pub fn peek(&self) -> Option<&T> {
         if let Some(rc) = &self.0 {
-            Some(&rc.0)
+            Some(&rc.value)
         } else {
             None
         }
@@ -23,7 +40,7 @@ impl<T> Stack<T> {
 
     pub fn push(&mut self, value: T) {
         let this = Self(self.0.take());
-        self.0 = Some(Rc::new((value, this)));
+        self.0 = Some(Rc::new(Item::new(value, this)));
     }
 }
 
@@ -31,9 +48,9 @@ impl<T: Clone> Stack<T> {
     pub fn pop(&mut self) -> Option<T> {
         let this = Self(self.0.take());
         if let Some(rc) = this.0 {
-            let (head, tail) = Rc::try_unwrap(rc).unwrap_or_else(|rc| (*rc).clone());
-            *self = tail;
-            Some(head)
+            let item = Rc::try_unwrap(rc).unwrap_or_else(|rc| (*rc).clone());
+            *self = item.prev;
+            Some(item.value)
         } else {
             None
         }
