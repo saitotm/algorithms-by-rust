3f2a67c05a39e44ec06065520ca9af8d112252db
saitotm <tomokazu.ml.tech@gmail.com>
2022-02-07 22:34:39 +0900

    change struct stack

--- src/basic/stack.rs
+++ src/basic/stack.rs
@@ -1,6 +1,8 @@
 use std::rc::Rc;
 
-pub struct Stack<T> (Option<Rc<Item<T>>>);
+pub struct Stack<T> {
+    item: Option<Rc<Item<T>>>,
+} 
 
 struct Item<T> {
     value: T,
@@ -21,30 +23,30 @@ impl<T: Clone> Clone for Item<T> {
 
 impl<T> Clone for Stack<T> {
     fn clone(&self) -> Self {
-        Self(self.0.clone())
+        Self{ item: self.item.clone() }
     }
 }
 
 impl<T> Stack<T> {
     pub fn new() -> Self {
-        Self(None)
+        Self{item: None}
     }
     
     pub fn peek(&self) -> Option<&T> {
-        self.0.as_ref().map(|rc| &rc.value)
+        self.item.as_ref().map(|rc| &rc.value)
     }
 
     pub fn push(&mut self, value: T) {
-        let this = Self(self.0.take());
-        self.0 = Some(Rc::new(Item::new(value, this)));
+        let this = Self{ item: self.item.take() };
+        self.item = Some(Rc::new(Item::new(value, this)));
     }
 }
 
 impl<T: Clone> Stack<T> {
     pub fn pop(&mut self) -> Option<T> {
-        let this = Self(self.0.take());
+        let this = Self{item: self.item.take()};
 
-        this.0.map(|rc| {
+        this.item.map(|rc| {
             let item = Rc::try_unwrap(rc).unwrap_or_else(|rc| (*rc).clone());
             *self = item.prev;
             item.value
