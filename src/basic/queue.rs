b517c080f12faf2856e5f2d7349fe51e9ba90022
saitotm <tomokazu.ml.tech@gmail.com>
2022-02-10 10:33:18 +0900

    change the node pointer type from rc to box

--- src/basic/queue.rs
+++ src/basic/queue.rs
@@ -1,24 +1,21 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
 pub struct Queue<T> {
-    head: Option<Rc<RefCell<Node<T>>>>,
+    head: Option<Box<Node<T>>>,
 }
 
 struct Node<T> {
-    value: Rc<T>,
-    next: Option<Rc<RefCell<Self>>>,
+    value: T,
+    next: Option<Box<Self>>,
 }
 
 impl<T> Node<T> {
     fn new(value: T) -> Self {
-        Self { value: Rc::new(value), next: None }
+        Self { value: value, next: None }
     }
 
     fn push(&mut self, value: T) {
         match self.next {
-            Some(ref mut next) => { next.borrow_mut().push(value) },
-            None => { self.next = Some(Rc::new(RefCell::new(Node::new(value)))) },
+            Some(ref mut next) => { next.push(value) },
+            None => { self.next = Some(Box::new(Node::new(value))) },
         }
     }
 }
@@ -28,31 +25,29 @@ impl<T> Queue<T> {
         Self { head: None }
     }
 
-    pub fn peek(&self) -> Option<Rc<T>> {
-        self.head.as_ref().map(|h| h.borrow().value.clone())
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|h| &h.value)
     }
 
     pub fn push(&mut self, value: T) {
         match self.head {
-            None => self.head = Some(Rc::new(RefCell::new(Node::new(value)))),
-            Some(ref mut h) => h.borrow_mut().push(value),
+            None => self.head = Some(Box::new(Node::new(value))),
+            Some(ref mut h) => h.push(value),
         }
     }
 
-    pub fn pop(&mut self) -> Option<Rc<T>> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take()
         .map(|ret| {
-            let ret = ret.borrow();
-            self.head = ret.next.clone();
+            self.head = ret.next;
 
-            ret.value.clone()
+            ret.value
         })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::rc::Rc;
     use super::Queue;
 
     #[test] 
@@ -64,14 +59,12 @@ mod tests {
         que.push(9);
 
         let que_peek_5 = que.peek();
-        assert_eq!(que_peek_5, Some(Rc::new(5)));
+        assert_eq!(que_peek_5, Some(&5));
 
-        assert_eq!(que.pop(), Some(Rc::new(5)));
-        assert_eq!(que.pop(), Some(Rc::new(2)));
-        assert_eq!(que.pop(), Some(Rc::new(9)));
+        assert_eq!(que.pop(), Some(5));
+        assert_eq!(que.pop(), Some(2));
+        assert_eq!(que.pop(), Some(9));
         assert_eq!(que.pop(), None);
         assert_eq!(que.pop(), None);
-
-        assert_eq!(que_peek_5, Some(Rc::new(5)));
     }
 }
