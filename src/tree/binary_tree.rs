b1382879e8193450813605b4a5214605c4deba6e
saitotm <tomokazu.ml.tech@gmail.com>
2022-02-10 15:04:56 +0900

    fix test

--- src/tree/binary_tree.rs
+++ src/tree/binary_tree.rs
@@ -26,22 +26,22 @@ mod tests {
 
     #[test]
     fn test_binary_tree() {
-        let mut btree = BinaryTree::new();
-        assert_eq!(btree.find(&3), None);
+        let mut binary_tree = BinaryTree::new();
+        assert_eq!(binary_tree.find(&3), None);
 
-        btree.add(7);
-        btree.add(5);
-        btree.add(3);
+        binary_tree.add(7);
+        binary_tree.add(5);
+        binary_tree.add(3);
 
-        assert_eq!(btree.find(&3), Some(&3));
-        assert_eq!(btree.find(&5), Some(&3));
-        assert_eq!(btree.find(&7), Some(&7));
-        assert_eq!(btree.find(&9), None);
+        assert_eq!(binary_tree.find(&3), Some(&3));
+        assert_eq!(binary_tree.find(&5), Some(&3));
+        assert_eq!(binary_tree.find(&7), Some(&7));
+        assert_eq!(binary_tree.find(&9), None);
 
-        btree.remove(&3);
+        binary_tree.remove(&3);
 
-        assert_eq!(btree.find(&3), None);
-        assert_eq!(btree.find(&5), Some(&3));
-        assert_eq!(btree.find(&7), Some(&7));
+        assert_eq!(binary_tree.find(&3), None);
+        assert_eq!(binary_tree.find(&5), Some(&3));
+        assert_eq!(binary_tree.find(&7), Some(&7));
     }
 }
