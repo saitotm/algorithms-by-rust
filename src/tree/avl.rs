f49bc7e599bd0a9561496eb50aec1db4887c246b
saitotm <tomokazu.ml.tech@gmail.com>
2022-03-17 10:54:51 +0900

    add struct NodeOption

--- src/tree/avl.rs
+++ src/tree/avl.rs
@@ -1,23 +1,28 @@
+// TODO: define struct Balance
 use std::cmp;
 use std::cmp::Ordering;
 use std::mem;
 
-struct AVL<T: Ord> {
-    root: NodeOpt<T>,
+pub struct AVL<T: Ord> {
+    root: NodeOption<T>,
+}
+
+struct NodeOption<T: Ord> {
+    node_opt: Option<Box<Node<T>>>,
 }
 
 struct Node<T: Ord> {
     value: T,
-    lhs: NodeOpt<T>,
-    rhs: NodeOpt<T>,
+    lhs: NodeOption<T>,
+    rhs: NodeOption<T>,
 }
-
-type NodeOpt<T> = Option<Box<Node<T>>>;
 
 impl<T: Ord> AVL<T> {
     /// Constructs a new, empty AVL<T>.
     pub fn new() -> Self {
-        Self { root: None }
+        Self {
+            root: NodeOption::new(),
+        }
     }
 
     /// Creates a AVL<T> from  slice
@@ -36,12 +41,12 @@ impl<T: Ord> AVL<T> {
     /// If the value is found then Option::Some is returned, containing the matching
     /// value. If the value is not found then Option::None is returned.
     pub fn find(&self, value: &T) -> Option<&T> {
-        self.root.as_ref()?.find(value)
+        self.root.find(value)
     }
 
     /// Adds a node containing a given value.
     pub fn add(&mut self, value: T) {
-        Node::add(&mut self.root, value);
+        self.root.add(value);
     }
 
     /// Removes a node containing a given value.
@@ -50,60 +55,64 @@ impl<T: Ord> AVL<T> {
     /// is returned, containing the matching value. If the value is not found then
     /// Option::None is returned.
     pub fn remove(&mut self, value: &T) -> Option<T> {
-        Node::remove(&mut self.root, value)
+        self.root.remove(value)
     }
 }
 
-impl<T: Ord> Node<T> {
-    fn new(value: T) -> Self {
+impl<T: Ord> NodeOption<T> {
+    fn new() -> Self {
+        Self { node_opt: None }
+    }
+
+    fn from_node(node: Node<T>) -> Self {
         Self {
-            value,
-            lhs: None,
-            rhs: None,
+            node_opt: Some(Box::new(node)),
         }
     }
 
     fn find(&self, value: &T) -> Option<&T> {
-        match value.cmp(&self.value) {
-            Ordering::Equal => Some(&self.value),
-            Ordering::Less => self.lhs.as_ref()?.find(value),
-            Ordering::Greater => self.rhs.as_ref()?.find(value),
+        let node = self.as_ref()?;
+
+        match value.cmp(&node.value) {
+            Ordering::Equal => Some(&node.value),
+            Ordering::Less => node.lhs.find(value),
+            Ordering::Greater => node.rhs.find(value),
         }
     }
 
-    fn add(node_opt: &mut NodeOpt<T>, value: T) {
-        match node_opt {
-            Some(ref mut node) => match value.cmp(&node.value) {
+    fn add(&mut self, value: T) {
+        match self.as_mut() {
+            Some(node) => match value.cmp(&node.value) {
                 Ordering::Less => {
-                    Node::add(&mut node.lhs, value);
-                    Self::rebalance(node_opt);
+                    node.lhs.add(value);
+                    self.rebalance();
                 }
                 Ordering::Greater => {
-                    Node::add(&mut node.rhs, value);
-                    Self::rebalance(node_opt);
+                    node.rhs.add(value);
+                    self.rebalance();
                 }
                 Ordering::Equal => (),
             },
-            None => *node_opt = Some(Box::new(Node::new(value))),
+            None => self.node_opt = Some(Box::new(Node::new(value))),
         }
     }
 
-    fn remove(node_opt: &mut NodeOpt<T>, value: &T) -> Option<T> {
-        if let Some(ref mut node) = node_opt {
+    fn remove(&mut self, value: &T) -> Option<T> {
+        if let Some(ref mut node) = self.as_mut() {
             let result = match value.cmp(&node.value) {
                 Ordering::Less => {
                     let result = Self::remove(&mut node.lhs, value);
-                    Self::rebalance(node_opt);
+                    self.rebalance();
 
                     result
                 }
                 Ordering::Greater => {
                     let result = Self::remove(&mut node.rhs, value);
-                    Self::rebalance(node_opt);
+                    self.rebalance();
 
                     result
                 }
-                Ordering::Equal => Self::remove_self(node_opt, value),
+                Ordering::Equal => self.remove_self(value),
             };
 
             return result;
@@ -112,51 +121,53 @@ impl<T: Ord> Node<T> {
         None
     }
 
-    fn rebalance(node_opt: &mut NodeOpt<T>) {
-        if let Some(ref mut node) = node_opt {
+    fn rebalance(&mut self) {
+        if let Some(node) = self.as_mut() {
             let balance = node.get_balance();
 
             match balance {
                 2 => {
                     let rhs = node.rhs.as_ref().expect("The rhs of the node must be Option::Some because the balance of the node is positive.");
                     if rhs.get_balance() == -1 {
-                        Self::rotate_right(&mut node.rhs);
+                        node.rhs.rotate_right();
                     }
-                    Self::rotate_left(node_opt);
+                    self.rotate_left();
                 }
                 -2 => {
                     let lhs = node.lhs.as_ref().expect("The lhs of the node must be Option::Some because the balance of the node is negative.");
                     if lhs.get_balance() == 1 {
-                        Self::rotate_left(&mut node.lhs);
+                        node.lhs.rotate_left();
                     }
-                    Self::rotate_right(node_opt);
+                    self.rotate_right();
                 }
                 _ => (),
             }
         }
     }
 
-    // Removes the given node.
-    // The value of the given node_opt and the given value are required to be same.
-    fn remove_self(node_opt: &mut NodeOpt<T>, value: &T) -> Option<T> {
-        let mut node = node_opt
+    // Removes the root of tree (self).
+    // The value of the node and the given value are required to be same.
+    fn remove_self(&mut self, value: &T) -> Option<T> {
+        let mut node = self
             .take()
-            .expect("The given node_opt is required not to be Option::None.");
+            .expect("The node is required not to be Option::None.");
 
-        match (&mut node.lhs, &node.rhs) {
+        match (node.lhs.as_mut(), node.rhs.as_ref()) {
             (None, None) => {
-                *node_opt = Some(node);
-                node_opt.take().map(|n| n.value)
+                self.set(node);
+                self.take().map(|n| n.value)
             }
             (Some(_), None) => {
-                let lhs = node.lhs.take();
-                *node_opt = lhs;
+                if let Some(lhs) = node.lhs.take() {
+                    self.set(lhs);
+                }
 
                 Some(node.value)
             }
             (None, Some(_)) => {
-                let rhs = node.rhs.take();
-                *node_opt = rhs;
+                if let Some(rhs) = node.rhs.take() {
+                    self.set(rhs);
+                }
 
                 Some(node.value)
             }
@@ -165,10 +176,84 @@ impl<T: Ord> Node<T> {
                 mem::swap(&mut node.value, &mut lhs_max_node.value);
 
                 let result = Self::remove(&mut node.lhs, value);
-                *node_opt = Some(node);
-                Self::rebalance(node_opt);
+                self.set(node);
+                self.rebalance();
                 result
             }
+        }
+    }
+
+    // The node and the right child are required to be Option::Some.
+    fn rotate_left(&mut self) {
+        let mut node = self
+            .take()
+            .expect("The given node is required not to be None.");
+
+        let mut rhs = node
+            .rhs
+            .take()
+            .expect("The rhs of the given node is required not to be None.");
+
+        if let Some(n) = rhs.lhs.take() {
+            node.rhs.set(n);
+        }
+        rhs.lhs.set(node);
+
+        *self = NodeOption::from_node(*rhs);
+    }
+
+    // The node and the left child are required to be Option::Some.
+    fn rotate_right(&mut self) {
+        let mut node = self
+            .take()
+            .expect("The given node is required not to be None.");
+
+        let mut lhs = node
+            .lhs
+            .take()
+            .expect("The lhs of The given node is required not to be None.");
+
+        if let Some(d) = lhs.rhs.take() {
+            node.lhs.set(d);
+        }
+        lhs.rhs.set(node);
+
+        *self = NodeOption::from_node(*lhs);
+    }
+
+    // Returns the height of the given node.
+    // If the node is None, then 0 is returned.
+    // If the node is Some, then adding one to the bigger one of the children's heights.
+    fn get_height(&self) -> i32 {
+        match self.as_ref() {
+            Some(node) => cmp::max(node.lhs.get_height(), node.rhs.get_height()) + 1,
+            None => 0,
+        }
+    }
+
+    fn as_mut(&mut self) -> Option<&mut Box<Node<T>>> {
+        self.node_opt.as_mut()
+    }
+
+    fn as_ref(&self) -> Option<&Box<Node<T>>> {
+        self.node_opt.as_ref()
+    }
+
+    fn take(&mut self) -> Option<Box<Node<T>>> {
+        self.node_opt.take()
+    }
+
+    fn set(&mut self, node: Box<Node<T>>) {
+        self.node_opt = Some(node);
+    }
+}
+
+impl<T: Ord> Node<T> {
+    fn new(value: T) -> Self {
+        Self {
+            value,
+            lhs: NodeOption::new(),
+            rhs: NodeOption::new(),
         }
     }
 
@@ -176,54 +261,12 @@ impl<T: Ord> Node<T> {
     // If the right child is taller than left, then the return value is positive.
     // If the left child is taller than right, then the return value is negative.
     fn get_balance(&self) -> i32 {
-        Self::get_height(&self.rhs) - Self::get_height(&self.lhs)
-    }
-
-    // Returns the height of the given node.
-    // If the node is None, then 0 is returned.
-    // If the node is Some, then adding one to the bigger one of the children's heights.
-    fn get_height(node_opt: &NodeOpt<T>) -> i32 {
-        match node_opt {
-            Some(node) => cmp::max(Self::get_height(&node.lhs), Self::get_height(&node.rhs)) + 1,
-            None => 0,
-        }
-    }
-
-    // The given node_opt and the right child are required to be Option::Some.
-    fn rotate_left(node_opt: &mut NodeOpt<T>) {
-        let mut node = node_opt
-            .take()
-            .expect("The given node_opt is required not to be None.");
-        let mut rhs = node
-            .rhs
-            .take()
-            .expect("The rhs of the given node_opt is required not to be None.");
-
-        node.rhs = rhs.lhs.take();
-        rhs.lhs = Some(node);
-
-        *node_opt = Some(rhs);
-    }
-
-    // The given node_opt and the left child are required to be Option::Some.
-    fn rotate_right(node_opt: &mut NodeOpt<T>) {
-        let mut node = node_opt
-            .take()
-            .expect("The given node_opt is required not to be None.");
-        let mut lhs = node
-            .lhs
-            .take()
-            .expect("The lhs of The given node_opt is required not to be None.");
-
-        node.lhs = lhs.rhs.take();
-        lhs.rhs = Some(node);
-
-        *node_opt = Some(lhs);
+        self.rhs.get_height() - self.lhs.get_height()
     }
 
     // Returns the mutable reference to the node containing the greatest value in the tree.
     fn max_mut(&mut self) -> &mut Self {
-        match self.rhs {
+        match self.rhs.node_opt {
             Some(ref mut rhs) => rhs.max_mut(),
             None => self,
         }
@@ -232,14 +275,14 @@ impl<T: Ord> Node<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::NodeOpt;
+    use super::NodeOption;
     use super::AVL;
     use std::cmp::Ordering;
 
     const COMPLEX_TREE_SOURCE: [i32; 9] = [7, 5, 4, 2, 6, 11, 9, 10, 13];
 
-    fn is_valid_balance<T: Ord>(node_opt: &NodeOpt<T>) -> bool {
-        if let Some(node) = node_opt {
+    fn is_valid_balance<T: Ord>(node_opt: &NodeOption<T>) -> bool {
+        if let Some(ref node) = node_opt.node_opt {
             return match node.get_balance().cmp(&1) {
                 Ordering::Greater => false,
                 _ => is_valid_balance(&node.lhs) && is_valid_balance(&node.rhs),
@@ -249,15 +292,15 @@ mod tests {
         true
     }
 
-    fn is_valid_structure<T: Ord>(node_opt: &NodeOpt<T>) -> bool {
-        if let Some(node) = node_opt {
-            if let Some(ref lhs) = node.lhs {
+    fn is_valid_structure<T: Ord>(node_opt: &NodeOption<T>) -> bool {
+        if let Some(ref node) = node_opt.node_opt {
+            if let Some(ref lhs) = node.lhs.node_opt {
                 if !((lhs.value < node.value) && is_valid_structure(&node.lhs)) {
                     return false;
                 }
             }
 
-            if let Some(ref rhs) = node.rhs {
+            if let Some(ref rhs) = node.rhs.node_opt {
                 if !((rhs.value > node.value) && is_valid_structure(&node.rhs)) {
                     return false;
                 }
