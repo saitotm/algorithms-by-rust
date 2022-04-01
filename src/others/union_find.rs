1636bdea54484842ca652712c678426b3f63f6b2
saitotm <tomokazu.ml.tech@gmail.com>
2022-04-01 23:54:17 +0900

    add basic implementations

--- src/others/union_find.rs
+++ src/others/union_find.rs
@@ -3,20 +3,59 @@ struct UnionFind {
 }
 
 impl UnionFind {
-    fn root(&self, x: usize) -> usize {
-        unimplemented!();
+    fn new(size: usize) -> Self {
+        let mut parent_indexs = vec![0; size];
+        for (i, idx) in parent_indexs.iter_mut().enumerate().take(size) {
+            *idx = i;
+        }
+
+        Self { parent_indexs }    
+    }
+
+    fn root(&mut self, x: usize) -> usize {
+        if self.parent_indexs[x] == x {
+            x
+        } else {
+            self.parent_indexs[x] = self.root(self.parent_indexs[x]);
+            self.parent_indexs[x]
+        }
     }
 
     fn unite(&mut self, x: usize, y: usize) {
-        unimplemented!();
+        let rx = self.root(x);
+        let ry = self.root(y);
+
+        if rx != ry {
+            self.parent_indexs[rx] = ry;
+        }
     }
 
-    fn same(&self, x: usize, y: usize) -> bool {
-        unimplemented!();
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        let rx = self.root(x);
+        let ry = self.root(y);
+
+        rx == ry
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn unite_and_same() {
+        let mut uf = UnionFind::new(5);
+
+        assert!(!uf.same(0, 1));
+        assert!(!uf.same(0, 2));
+        assert!(!uf.same(3, 2));
+
+        uf.unite(0, 1);
+        uf.unite(3, 2);
+        assert!(uf.same(0, 1));
+        assert!(uf.same(3, 2));
+
+        uf.unite(1, 2);
+        assert!(uf.same(0, 3));
+    }
 }
